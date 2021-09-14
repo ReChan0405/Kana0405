c7635a6e6423d57b73dfc42325166b662c3cb5ad
ReChan0405 <90127790+ReChan0405@users.noreply.github.com>
2021-09-14 21:11:41 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,11 @@
+import discord
+from discord.ext import commands 
 
+Bot = commands.Bot(command_prefix = '#')
+
+@Bot.event
+async def on_ready():
+    print(f'{client.user} is ready ')
+    
+Bot.load_extension('cogs.CommandEvents')
+Bot.run('TOKEN')
