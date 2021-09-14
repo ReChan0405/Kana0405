536b085beda0719381855aa247caea37f86b8c10
ReChan0405 <90127790+ReChan0405@users.noreply.github.com>
2021-09-14 22:06:51 +0530

    Update CommandEvents.py

--- cogs/CommadnEvents.py
+++ cogs/CommadnEvents.py
@@ -5,21 +5,21 @@ class CommandEvents(commands.Cog):
   def __init__(self, bot):
     self.bot = bot 
     
-@client.event 
-async def on_command_error(ctx, error):
-  print(ctx.command.name +' was called incorrectly')
+  @bot.Cog.listener()
+  async def on_command_error(ctx, error):
+    print(ctx.command.name +' was called incorrectly')
   
-@client.event
-async def on_command(ctx):
-  if ctx.command is not None:
-    if ctx.command.name in commands_tally:
-      commands_tally[ctx.command.name] += 1
-    else:
-      commands_tally[ctx.command.name] = 1
+  @bot.Cog.listener()
+  async def on_command(ctx):
+    if ctx.command is not None:
+      if ctx.command.name in commands_tally:
+        commands_tally[ctx.command.name] += 1
+      else:
+        commands_tally[ctx.command.name] = 1
       
-@client.event 
-async def on_command_completion(ctx):
-  print(ctx.command.name + 'was called successfully')
+  @bot.Cog.listener()
+  async def on_command_completion(ctx):
+    print(ctx.command.name + 'was called successfully')
 def setup(bot):
   bot.add_cog(CommandEvents(bot))
     
