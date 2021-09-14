223055da1453534955fcc1354781a8a4e6d57d14
ReChan0405 <90127790+ReChan0405@users.noreply.github.com>
2021-09-15 00:54:01 +0530

    Update HelpCommands.py

--- cogs/HelpCommands.py
+++ cogs/HelpCommands.py
@@ -1,8 +1,21 @@
 from discord import commands 
+from discord import colour
 
 class HelpCommands(commands.Cog):
   def __init__(self, bot):
-    self.bot = bot 
+    self.bot = bot
+  
+  @commands.group(invoke_without_command=True)
+  async def help(self, ctx):
+    em = discord.Embed(title = 'help', description = 'Use a!help command for extended information on the command.',color = ctx.author.color)
+
+    em.add_field(name = 'economy', value = 'bag , balance , beg , buy , deposits , leaderboard , rob , sell , send , shop , slot , withdraw')
+    em.add_field(name= 'stats' , value = 'ping , graph')
+    em.add_field(name = 'moderation', value = 'kick , ban , mute , purge , unban , unmute , info ')
+    
+    await ctx.send(embed = em)
+  
+  
     
     
 def setup(bot):
