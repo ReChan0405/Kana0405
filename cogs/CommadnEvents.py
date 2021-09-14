502ff9512e770b848157ed7bb7ba87009c881066
ReChan0405 <90127790+ReChan0405@users.noreply.github.com>
2021-09-14 22:03:55 +0530

    Create CommandEvents.py

---
+++ cogs/CommadnEvents.py
@@ -0,0 +1,25 @@
+import discord
+from discord.ext import commands 
+
+class CommandEvents(commands.Cog):
+  def __init__(self, bot):
+    self.bot = bot 
+    
+@client.event 
+async def on_command_error(ctx, error):
+  print(ctx.command.name +' was called incorrectly')
+  
+@client.event
+async def on_command(ctx):
+  if ctx.command is not None:
+    if ctx.command.name in commands_tally:
+      commands_tally[ctx.command.name] += 1
+    else:
+      commands_tally[ctx.command.name] = 1
+      
+@client.event 
+async def on_command_completion(ctx):
+  print(ctx.command.name + 'was called successfully')
+def setup(bot):
+  bot.add_cog(CommandEvents(bot))
+    
